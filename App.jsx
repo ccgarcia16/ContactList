3c1ea035e46a927eb4f04b917c34c3de9d32b03c
judey <cg@judeys-air.mynetworksettings.com>
2024-03-27 14:23:21 -0400

    Finished my workshop. Will submit then try to reach the stretch goals.

--- App.jsx
+++ App.jsx
@@ -1,10 +1,48 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [contacts, setContacts] = useState([]);
+  const API_URL = 'https://jsonplaceholder.typicode.com/users';
 
-  return 
-}
+  useEffect(() => {
+    const getContacts = async() => {
+      const response = await fetch(API_URL);
+      const json = await response.json();
+      const contactList = json;
+      setContacts(contactList);
+    }
+    getContacts();
+  },[]);
+
+  return (
+    <>
+      <h1>Contact List</h1>
+      <div className='divContainer'>
+        <ul>
+          {
+            contacts.map((singleContact) => {
+              return <li>{singleContact.name}</li>
+          })}
+          </ul>
+
+        <ul>
+          {
+            contacts.map((singleContact) => {
+              return <li>{singleContact.email}</li>
+            })
+          }
+        </ul>
+
+        <ul>
+          {
+            contacts.map((singleContact) => {
+              return <li>{singleContact.phone}</li>
+            })
+          }
+        </ul>
+      </div>
+    </>
+  )}
 
 export default App
